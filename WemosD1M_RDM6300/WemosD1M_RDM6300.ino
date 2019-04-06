f34f710dc103620d90f35d4f3d41d9d5b5b03232
gundarsv <267009@viauc.dk>
2019-04-07 00:02:07 +0200

    Added project

---
+++ WemosD1M_RDM6300/WemosD1M_RDM6300.ino
@@ -0,0 +1,133 @@
+#include <rdm6300.h>
+#define RDM6300_RX_PIN 2
+#define READ_LED_PIN 13
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <SoftwareSerial.h>
+
+#ifndef STASSID
+#define STASSID "WIFI"
+#define STAPSK  "PASSWORD"
+#endif
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
+
+const char* host = "fillagain.azurewebsites.net";
+const int httpsPort = 443;
+
+// Use web browser to view and copy
+// SHA1 fingerprint of the certificate
+const char fingerprint[] PROGMEM = "3ab0b1c27f746fd90c34f0d6a960cf73a4229de8";
+Rdm6300 rdm6300;
+bool flop = false;
+
+
+
+
+
+void setup()
+{
+	Serial.begin(115200);
+  WiFiCon();
+	pinMode(READ_LED_PIN, OUTPUT);
+	digitalWrite(READ_LED_PIN, LOW);
+
+	rdm6300.begin(RDM6300_RX_PIN);
+
+	Serial.println("Place RFID tag near the rdm6300...");
+}
+
+void loop()
+{
+  if (WiFiCon() > 0) {
+    if (rdm6300.update())
+      {
+        postData();
+      }
+      digitalWrite(READ_LED_PIN, rdm6300.is_tag_near());
+  }
+	
+	delay(1000);
+}
+
+int WiFiCon() {
+    // Check if we have a WiFi connection, if we don't, connect.
+  int xCnt = 0;
+
+  if (WiFi.status() != WL_CONNECTED){
+
+        Serial.println();
+        Serial.println();
+        Serial.print("Connecting to ");
+        Serial.println(ssid);
+
+        WiFi.mode(WIFI_STA);
+        
+        WiFi.begin(ssid, password);
+        
+        while (WiFi.status() != WL_CONNECTED  && xCnt < 50) {
+          delay(500);
+          Serial.print(".");
+          xCnt ++;
+        }
+
+        if (WiFi.status() != WL_CONNECTED){
+          Serial.println("WiFiCon=0");
+          return 0; //never connected
+        } else {
+          Serial.println("WiFiCon=1");
+          Serial.println("");
+          Serial.println("WiFi connected");  
+          Serial.println("IP address: ");
+          Serial.println(WiFi.localIP());
+          return 1; //1 is initial connection
+        }
+
+  } else {
+    Serial.println("WiFiCon=2");
+    return 2; //2 is already connected
+  
+  }
+}
+void postData() {
+  WiFiClientSecure client;
+  Serial.print("connecting to ");
+  Serial.println(host);
+
+  Serial.printf("Using fingerprint '%s'\n", fingerprint);
+  client.setFingerprint(fingerprint);
+
+  if (!client.connect(host, httpsPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  String url = "/api/cups/22";
+  Serial.print("requesting URL: ");
+  Serial.println(url);
+
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "User-Agent: BuildFailureDetectorESP8266\r\n" +
+               "Connection: close\r\n\r\n");
+
+  Serial.println("request sent");
+  while (client.connected()) {
+    String line = client.readStringUntil('\n');
+    if (line == "\r") {
+      Serial.println("headers received");
+      break;
+    }
+  }
+  String line = client.readStringUntil('\n');
+  if (line.startsWith("{\"state\":\"success\"")) {
+    Serial.println("esp8266/Arduino CI successfull!");
+  } else {
+    Serial.println("esp8266/Arduino CI has failed");
+  }
+  Serial.println("reply was:");
+  Serial.println("==========");
+  Serial.println(line);
+  
+  }
